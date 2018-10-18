35486d046dcc4797e257a31a886371f5daadda4c
Christian Blythe <ucfblythe83@gmail.com>
2018-10-17 21:00:14 -0400

    Committing Range Query Definition and test change.

--- src/com/cop6616/lfcat/RangeNode.java
+++ src/com/cop6616/lfcat/RangeNode.java
@@ -1,5 +1,7 @@
 package com.cop6616.lfcat;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class RangeNode<T extends Comparable<T>> extends Node
 {
     int lokey;
@@ -42,5 +44,20 @@ public class RangeNode<T extends Comparable<T>> extends Node
         }
 
         return stat;
+    }
+
+    public static boolean AllInRange(AtomicReference<Node> root, int low, int key)
+    {
+        /*Given a range to find, the Range query operation traverses the structure while saving the traversal in stacks
+        and marking relevant nodes as part of the Range Query. The operation begins by traversing the LFCAT structure to
+        find the Base node containing the low key in the range. The low Base node, and the parent used to reach it in the
+        stack, are marked as part of the Range Query by CAS replacing their nodes with Range node copies. This Base node
+        is tracked in a stack of visited nodes that are know to be part of the range. Starting from this Base node this
+        operation is repeated on the subsequent furthest left nodes that have yet to be visited. When a Base node that
+        contains or exceeds the high range key is reached then we have completed our full list of required base nodes,
+        and marked them and their parent as part of the range query. The stack of verified nodes is then traversed in
+        stack order in order to join each treap into one result set. */
+
+        return false;
     }
 }
