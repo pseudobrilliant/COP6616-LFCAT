b5d2d5e3af9fd9d14ed7112c47ac98e5c547a109
Christian Blythe <cblythe@knights.cuf.edu>
2018-10-28 20:22:19 -0400

    Changes to RangeNode to adapt goto logic in code.

--- src/com/cop6616/lfcat/RangeNode.java
+++ src/com/cop6616/lfcat/RangeNode.java
@@ -90,63 +90,77 @@ public class RangeNode<T extends Comparable<T>> extends BaseNode<T>
         }
     }
 
+    public static void ReplaceTop(Deque<Node> s, Node n)
+    {
+        s.pop();
+        s.push(n);
+    }
+
     public AVLTree<T> AllInRange(AtomicReference<Node> m, int lowKey, int highKey, ResultStorage<T> rs) //TODO: Add Result Set
     {
         Deque<Node> done = new ArrayDeque<Node>();
         Deque<Node> s = new ArrayDeque<Node>();
         Deque<Node> backup_s = new ArrayDeque<Node>();
-        ResultStorage mys;
-        Node b;
-        while(true)
+
+        return LowInRange(m, lowKey,highKey,s,backup_s,done,rs);
+
+    }
+
+    public AVLTree<T> LowInRange(AtomicReference<Node> m, int lowKey, int highKey,
+                                 Deque<Node> s, Deque<Node> backup_s, Deque<Node> done, ResultStorage<T> rs)
+    {
+        Node b = FindBaseStack(m.get(), lowKey, s);
+        ResultStorage<T> mys = null;
+        if (rs != null)
         {
-            done.clear();
-            s.clear();
-            backup_s.clear();
-            b = FindBaseStack(m.get(), lowKey, s);
-            mys = null;
-            if (rs != null)
+            if (b.type != NodeType.RANGE)
             {
-                if (b.type != NodeType.RANGE)
+                return rs.result.get();
+            }
+            else
+            {
+                RangeNode<T> brn = (RangeNode<T>) b;
+                if (brn.storage != rs)
                 {
                     return rs.result.get();
                 }
                 else
                 {
-                    RangeNode<T> brn = (RangeNode<T>) b;
-                    if (brn.storage != rs)
-                    {
-                        return rs.result.get();
-                    }
-                    else
-                    {
-                        mys = rs;
-                    }
+                    mys = rs;
                 }
-            }
-            else if (!b.IsReplaceable())
-            {
-                mys = new ResultStorage();
-                RangeNode<T> n = new RangeNode<T>(b, lowKey, highKey, mys);
-
-                if (!TryReplace(m, b, n))
-                {
-                    continue;
-                }
-
-                ReplaceTop(s, n);
-            }
-            else if (b.type == NodeType.RANGE && ((RangeNode<T>) b).hikey >= hikey)
-            {
-                RangeNode<T> brn = (RangeNode<T>) b;
-                return AllInRange(m, brn.lokey, brn.hikey, brn.storage);
-            }
-            else
-            {
-                //
-                continue;
             }
         }
+        else if (!b.IsReplaceable())
+        {
+            mys = new ResultStorage();
+            RangeNode<T> n = new RangeNode<T>(b, lowKey, highKey, mys);
 
+            if (!TryReplace(m, b, n))
+            {
+                //I believe this should be rs, as mys has no values yet, and we've failed the swap
+                return LowInRange(m, lowKey, highKey, s, backup_s, done, rs);
+            }
+
+            ReplaceTop(s, n);
+        }
+        else if (b.type == NodeType.RANGE && ((RangeNode<T>) b).hikey >= hikey)
+        {
+            RangeNode<T> brn = (RangeNode<T>) b;
+            return AllInRange(m, brn.lokey, brn.hikey, brn.storage);
+        }
+        else
+        {
+            //Help_If_Needed
+            return LowInRange(m, lowKey, highKey, s, backup_s, done, rs);
+        }
+
+        return RestInRange(m, lowKey, highKey, b, s, backup_s, done, mys);
+    }
+
+    public AVLTree<T> RestInRange(AtomicReference<Node> m, int lowKey, int highKey, Node b,
+                                  Deque<Node> s, Deque<Node> backup_s, Deque<Node> done,
+                                  ResultStorage<T> mys)
+    {
         while(true)
         {
             done.push(b);
